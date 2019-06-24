0dec5500a6a713abc85d39f76c3bc2ca7c18e4e6
bitcamp07 <bitcamp07@bitcamp>
2019-06-24 11:29:06 +0900

    0624 오전1 날짜띄우기/@등등등

--- sp1/src/main/java/org/zerock/controller/HomeController.java
+++ sp1/src/main/java/org/zerock/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.zerock.service.HelloService;
@@ -23,6 +24,12 @@ public class HomeController {
 	
 	@Autowired
 	private HelloService service;
+	
+	@GetMapping("/doA")
+	public String doA() {
+		logger.info("doA.........................");
+		return "mydoA";
+	}
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
